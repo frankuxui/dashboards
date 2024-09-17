abf160a1677039ffea02b52faffb0573ba59712b
frankuxui <fisdray@gmail.com>
2024-09-17 22:55:22 +0200

    fixed deploy

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+
+  output: {
+    assetPrefix: '/dashboards/',
+  },
+
   server: {
     htmlFallback: 'index',
     publicDir: [
